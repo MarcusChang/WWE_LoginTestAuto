5b8452639a97f88445cc441584a62107b424d985
Marcus Chang <marcus_chang@aliyun.com>
2016-04-14 00:04:13 +0800

    WWE QA Hiring Test Commit
    Third commit for the solution
    Added IE & Firefox Valid Cases !
    
    Marcus_Chang
    
    2016-APR-14

--- src/test/java/com/outlook/test/BPM/TestCase_Outlook_Login_Valid_On_Firefox.java
+++ src/test/java/com/outlook/test/BPM/TestCase_Outlook_Login_Valid_On_Firefox.java
@@ -1,21 +1,23 @@
 package com.outlook.test.BPM;
 
-import com.outlook.test.BPM.PageFactory.*;
-import com.outlook.test.BPM.TestParams.*;
+import com.outlook.test.BPM.PageFactory.Outlook_Page_Dashboard;
+import com.outlook.test.BPM.PageFactory.Outlook_Page_Login;
+import com.outlook.test.BPM.TestParams.BaseTest;
+import com.outlook.test.BPM.TestParams.ProjectParams;
 import com.outlook.test.BPM.Utils.*;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Created by Marcus_Chang on 2016/4/13.
  */
-public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
+public class TestCase_Outlook_Login_Valid_On_Firefox extends BaseTest {
 
-    private static WebDriver chromeDriver;
+    private static WebDriver firefoxDriver;
     private static Outlook_Page_Login OutlookPageLogin;
     private static Outlook_Page_Dashboard OutlookPageDashboard;
     private static ReportsUtils report;
@@ -25,48 +27,48 @@ public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
 
 
     @Rule
-    public RetryTest retry = new RetryTest(3, chromeDriver, logger, capture);
+    public RetryTest retry = new RetryTest(3, firefoxDriver, logger, capture);
 
     @BeforeClass
     public static void init() {
         //create the testUtilFunctions instance
         testUtil = new TestUtilFunctions();
         //initial DriverUtilFunctions.class，set the driver boot path in its constructor
-        new DriverUtilFunctions("Chrome");
-        DesiredCapabilities chromeCapabilities = testUtil.setChromeDriverProperties();
+        new DriverUtilFunctions("Firefox");
+        DesiredCapabilities firefoxCapabilities = testUtil.setFirefoxDriverProperties();
 
-        //create a Chrome browser instance
-        chromeDriver = new ChromeDriver(chromeCapabilities);
+        //create a Firefox browser instance
+        firefoxDriver = new FirefoxDriver(firefoxCapabilities);
         //Create the ReportsUtils instance
         report = new ReportsUtils();
         //Create the ScreenShotTaker instance
         capture = new ScreenShotTaker();
         //Begin to log the report details
-        logger = report.testLogger("TestCase_Outlook_Login_Valid_On_Chrome");
+        logger = report.testLogger("TestCase_Outlook_Login_Valid_On_Firefox");
         //initial LogUtilFunctions.class，set the system level info log configuration by create the class instance
         LogUtilFunctions logUtil = new LogUtilFunctions();
-        logUtil.setLoggerProperties("TestCase_Outlook_Login_Valid_On_Chrome", ProjectParams.getTestCase_Outlook_Login_Valid_On_Chrome_LayOut(), ProjectParams.getChromeDriverLocalPath());
+        logUtil.setLoggerProperties("TestCase_Outlook_Login_Valid_On_Firefox", ProjectParams.getTestCase_Outlook_Login_Valid_On_Firefox_LayOut(), ProjectParams.getFireFoxDriverLocalPath());
 
         OutlookPageLogin = new Outlook_Page_Login();
         OutlookPageDashboard = new Outlook_Page_Dashboard();
 
         //Maximize the browser window
-        OutlookPageLogin.MaxPageWindow(chromeDriver);
+        OutlookPageLogin.MaxPageWindow(firefoxDriver);
     }
 
     @Test
-    public void TestAction_Outlook_Login_Valid_On_Chrome() {
+    public void TestAction_Outlook_Login_Valid_On_Firefox() {
 
         //Action 1 : Open the Outlook login page
         logger.log(LogStatus.INFO, "Begin the test and open the test login page.");
-        OutlookPageLogin.OpenTargetURL(chromeDriver, ProjectParams.getOutlookURL());
+        OutlookPageLogin.OpenTargetURL(firefoxDriver, ProjectParams.getOutlookURL());
         //Grab all target web elements on the page : Outlook_Page_Login
-        OutlookPageLogin.getOutlookPageLoginElements(chromeDriver);
+        OutlookPageLogin.getOutlookPageLoginElements(firefoxDriver);
         //Assert the page title == [登陆]
         Assert.assertTrue(OutlookPageLogin.Txt_PageTitle.equals("登录"));
         logger.log(LogStatus.PASS, "Outlook Login Page Opened!");
         //Input the user name & pass to login
-        OutlookPageLogin.cleanLoginPageInputTextArea(chromeDriver, testUtil);
+        OutlookPageLogin.cleanLoginPageInputTextArea(firefoxDriver, testUtil);
         OutlookPageLogin.Ipt_UserName.sendKeys(ProjectParams.getLoginUserName_1());
         OutlookPageLogin.Ipt_PassWord.sendKeys(ProjectParams.getLoginUserPass_1());
         OutlookPageLogin.Btn_Login.click();
@@ -74,7 +76,7 @@ public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
         //Action 2 : verify the login action
         logger.log(LogStatus.INFO, "Login the Outlook Dashboard and prepare to grab target elements on the page.");
         //Grab all target web elements on the page : Outlook_Page_Dashboard
-        OutlookPageDashboard.getOutlookPageDashboardElements(chromeDriver);
+        OutlookPageDashboard.getOutlookPageDashboardElements(firefoxDriver);
         //Assert the page title == [Outlook.com - loginUser1]
         Assert.assertTrue(OutlookPageDashboard.Txt_PageTitle.equals("Outlook.com - " + ProjectParams.getLoginUserName_1()));
         logger.log(LogStatus.PASS, "Login Success! Outlook Dashboard Opened. TestAction_Login -> Finished!");
@@ -85,7 +87,7 @@ public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
     @AfterClass
     public static void tearDown() {
         report.endTestLogger(logger);
-        chromeDriver.quit();
+        firefoxDriver.quit();
     }
 
 
--- src/test/java/com/outlook/test/BPM/TestCase_Outlook_Login_Valid_On_IE.java
+++ src/test/java/com/outlook/test/BPM/TestCase_Outlook_Login_Valid_On_IE.java
@@ -1,21 +1,23 @@
 package com.outlook.test.BPM;
 
-import com.outlook.test.BPM.PageFactory.*;
-import com.outlook.test.BPM.TestParams.*;
+import com.outlook.test.BPM.PageFactory.Outlook_Page_Dashboard;
+import com.outlook.test.BPM.PageFactory.Outlook_Page_Login;
+import com.outlook.test.BPM.TestParams.BaseTest;
+import com.outlook.test.BPM.TestParams.ProjectParams;
 import com.outlook.test.BPM.Utils.*;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import org.junit.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Created by Marcus_Chang on 2016/4/13.
  */
-public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
+public class TestCase_Outlook_Login_Valid_On_IE extends BaseTest {
 
-    private static WebDriver chromeDriver;
+    private static WebDriver ieDriver;
     private static Outlook_Page_Login OutlookPageLogin;
     private static Outlook_Page_Dashboard OutlookPageDashboard;
     private static ReportsUtils report;
@@ -25,48 +27,48 @@ public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
 
 
     @Rule
-    public RetryTest retry = new RetryTest(3, chromeDriver, logger, capture);
+    public RetryTest retry = new RetryTest(3, ieDriver, logger, capture);
 
     @BeforeClass
     public static void init() {
         //create the testUtilFunctions instance
         testUtil = new TestUtilFunctions();
         //initial DriverUtilFunctions.class，set the driver boot path in its constructor
-        new DriverUtilFunctions("Chrome");
-        DesiredCapabilities chromeCapabilities = testUtil.setChromeDriverProperties();
+        new DriverUtilFunctions("IE");
+        DesiredCapabilities ieCapabilities = testUtil.setIEDriverProperties();
 
-        //create a Chrome browser instance
-        chromeDriver = new ChromeDriver(chromeCapabilities);
+        //create a IE browser instance
+        ieDriver = new InternetExplorerDriver(ieCapabilities);
         //Create the ReportsUtils instance
         report = new ReportsUtils();
         //Create the ScreenShotTaker instance
         capture = new ScreenShotTaker();
         //Begin to log the report details
-        logger = report.testLogger("TestCase_Outlook_Login_Valid_On_Chrome");
+        logger = report.testLogger("TestCase_Outlook_Login_Valid_On_IE");
         //initial LogUtilFunctions.class，set the system level info log configuration by create the class instance
         LogUtilFunctions logUtil = new LogUtilFunctions();
-        logUtil.setLoggerProperties("TestCase_Outlook_Login_Valid_On_Chrome", ProjectParams.getTestCase_Outlook_Login_Valid_On_Chrome_LayOut(), ProjectParams.getChromeDriverLocalPath());
+        logUtil.setLoggerProperties("TestCase_Outlook_Login_Valid_On_IE", ProjectParams.getTestCase_Outlook_Login_Valid_On_IE_LayOut(), ProjectParams.getIEDriverLocalPath());
 
         OutlookPageLogin = new Outlook_Page_Login();
         OutlookPageDashboard = new Outlook_Page_Dashboard();
 
         //Maximize the browser window
-        OutlookPageLogin.MaxPageWindow(chromeDriver);
+        OutlookPageLogin.MaxPageWindow(ieDriver);
     }
 
     @Test
-    public void TestAction_Outlook_Login_Valid_On_Chrome() {
+    public void TestAction_Outlook_Login_Valid_On_IE() {
 
         //Action 1 : Open the Outlook login page
         logger.log(LogStatus.INFO, "Begin the test and open the test login page.");
-        OutlookPageLogin.OpenTargetURL(chromeDriver, ProjectParams.getOutlookURL());
+        OutlookPageLogin.OpenTargetURL(ieDriver, ProjectParams.getOutlookURL());
         //Grab all target web elements on the page : Outlook_Page_Login
-        OutlookPageLogin.getOutlookPageLoginElements(chromeDriver);
+        OutlookPageLogin.getOutlookPageLoginElements(ieDriver);
         //Assert the page title == [登陆]
         Assert.assertTrue(OutlookPageLogin.Txt_PageTitle.equals("登录"));
         logger.log(LogStatus.PASS, "Outlook Login Page Opened!");
         //Input the user name & pass to login
-        OutlookPageLogin.cleanLoginPageInputTextArea(chromeDriver, testUtil);
+        OutlookPageLogin.cleanLoginPageInputTextArea(ieDriver, testUtil);
         OutlookPageLogin.Ipt_UserName.sendKeys(ProjectParams.getLoginUserName_1());
         OutlookPageLogin.Ipt_PassWord.sendKeys(ProjectParams.getLoginUserPass_1());
         OutlookPageLogin.Btn_Login.click();
@@ -74,7 +76,7 @@ public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
         //Action 2 : verify the login action
         logger.log(LogStatus.INFO, "Login the Outlook Dashboard and prepare to grab target elements on the page.");
         //Grab all target web elements on the page : Outlook_Page_Dashboard
-        OutlookPageDashboard.getOutlookPageDashboardElements(chromeDriver);
+        OutlookPageDashboard.getOutlookPageDashboardElements(ieDriver);
         //Assert the page title == [Outlook.com - loginUser1]
         Assert.assertTrue(OutlookPageDashboard.Txt_PageTitle.equals("Outlook.com - " + ProjectParams.getLoginUserName_1()));
         logger.log(LogStatus.PASS, "Login Success! Outlook Dashboard Opened. TestAction_Login -> Finished!");
@@ -85,7 +87,7 @@ public class TestCase_Outlook_Login_Valid_On_Chrome extends BaseTest {
     @AfterClass
     public static void tearDown() {
         report.endTestLogger(logger);
-        chromeDriver.quit();
+        ieDriver.quit();
     }
 
 
